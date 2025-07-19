09d23beb99e01901ba393e2bf6949b0c01ca6513
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-19 17:58:38 -0500

    Find the larger of two numbers using System.Math class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+﻿// Exercise - Complete a challenge activity to discover and implement a method call
+// Find a method of the System.Math class that returns the larger of two numbers.
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue,secondValue);
 
-
-dice.Next();
+Console.WriteLine(largerValue);
