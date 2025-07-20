440a091b921525d2c72fab2be5c919849e8cad18
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-19 23:26:14 -0500

    Improve dice game using if-else conditions

--- Program.cs
+++ Program.cs
@@ -1,8 +1,52 @@
-﻿// Exercise - Complete a challenge activity to discover and implement a method call
-// Find a method of the System.Math class that returns the larger of two numbers.
+﻿// Exercise - Create decision logic with if statements
+// Program a dice game with if statement
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue,secondValue);
 
-Console.WriteLine(largerValue);
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+// int roll1 = 6;
+// int roll2 = 6;
+// int roll3 = 6;
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total");
+    total += 6;
+}
+
+else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total");
+    total += 2;
+}
+
+Console.WriteLine($"Your total including the bonus: {total}");
+
+
+if (16 <= total)
+{
+    Console.WriteLine("You win a new car!");
+}
+
+else if (10 <= total)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+
+else if (7 == total)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
