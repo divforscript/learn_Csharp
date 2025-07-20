b1669e994e65ff7567ee76609ca873ca5d2bda54
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-19 23:57:00 -0500

    Improve bussines logic exercise

--- Program.cs
+++ Program.cs
@@ -6,31 +6,32 @@ int discountPercentage = 0;
 
 Console.WriteLine($"Days until expiration: {daysUntilExpiration}");
 
-if (daysUntilExpiration <= 10)
+if (daysUntilExpiration == 0)
 {
-    if (daysUntilExpiration == 0)
-    {
-        Console.WriteLine("Your subscription has expired.");
-    }
+    Console.WriteLine("Your subscription has expired.");
+}
 
-    else if (daysUntilExpiration == 1)
-    {
-        Console.WriteLine("Your subscription expires within a day!");
-        discountPercentage = 20;
-        Console.WriteLine($"Renew now and save {discountPercentage}%!");
-    }
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
 
-    else if (1 < daysUntilExpiration && daysUntilExpiration <= 5)
-    {
-        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-        discountPercentage = 10;
-        Console.WriteLine($"Renew now and save {discountPercentage}%!");
-    }
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
 
-    else
-    {
-        Console.WriteLine($"Your subscription will expire soon. Renew now!");
-    }
+}
+
+else if(daysUntilExpiration <= 10)
+{
+    Console.WriteLine($"Your subscription will expire soon. Renew now!");
+}
+
+if (0 < discountPercentage)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
 }
 
 
