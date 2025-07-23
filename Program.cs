561e7d4802ecc096ba2640a68f419bf6140bc288
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-22 21:30:19 -0500

    22-07-25: Using comments

--- Program.cs
+++ Program.cs
@@ -1,21 +1,22 @@
-﻿// Exercise - Complete a challenge activity for nested iteration and selection statements
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
 
-string[] fraudulentOrderIDs =
-[   "B123",
-    "C234",
-    "A345",
-    "C15",
-    "B177",
-    "G3003",
-    "C235",
-    "B179"
-];
-
-
-foreach (string orderID in fraudulentOrderIDs)
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    if (orderID.StartsWith("B"))
-    {
-        Console.WriteLine(orderID);
-    }
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
 }
