7613893f3d1e94d7056218e5277bb4aa69e68f8a
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-25 16:24:20 -0500

    25-07-25: Project1 - write code that validates string input

--- Program.cs
+++ Program.cs
@@ -1,30 +1,35 @@
 ﻿// Exercise - Complete a challenge activity to differentiate between do and while iteration statements
 
-// Code project 1 - write code that validates integer input
+// Code project 2 - write code that validates string input
 
+string[] roles = new string[3] { "administrator", "manager", "user"};
 string? userInput;
-int numericValue = 0;
 bool validInput = false;
 
-Console.WriteLine("Enter an integer value between 5 and 10");
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
 
 do
 {
     userInput = Console.ReadLine();
-    validInput = int.TryParse(userInput, out numericValue);
+    userInput = userInput.Trim().ToLower();
 
-    if (!validInput)
+    for (int i = 0; i < roles.Length; i++)
     {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        if (roles[i] == userInput)
+        {
+            validInput = true;
+            break;
+        }
+    }
+
+    if (validInput)
         continue;
-    }
 
-    if (numericValue < 5 || 10 < numericValue)
-    {
-        validInput = false;
-        Console.WriteLine($"You entered {numericValue}. Please, enter an integer value between 5 and 10");
-    }
+    Console.WriteLine($"The role name that you entered, \"{userInput}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+
 
 } while (!validInput);
 
-Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+Console.WriteLine($"Your input value ({userInput}) has been accepted.");
+
+
