5ff40d490b3541a809a09e1306aeedfd12ae4ae7
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-25 09:29:48 -0500

    25-07-25: Learn about Do-While and While loop

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
-﻿// Exercise: Complete a challenge activity using for and if statements
+﻿// Exercise: Learn about Do-While and While loop
 
-string suffix = "";
+Random random = new Random();
+int current = 0;
 
-for (int i = 1; i <= 100; i++)
+do
 {
-    if (i % 15 == 0)
-        suffix = " - FizzBuzz";
-    else if (i % 3 == 0)
-        suffix = " - Fizz";
-    else if (i % 5 == 0)
-        suffix = " - Buzz";
-    else
-        suffix = "";
-
-    Console.WriteLine($"{i}{suffix}");
-}
-
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
