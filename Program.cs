f12f67efa98b3d8c11941a785be6cfcb430075d4
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-24 23:16:14 -0500

    24-07-25: Solve FizzBuzz challenge

--- Program.cs
+++ Program.cs
@@ -1,118 +1,18 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
+﻿// Exercise: Complete a challenge activity using for and if statements
 
-string[] product = sku.Split('-');
+string suffix = "";
 
-string type = "";
-string color = "";
-string size = "";
-
-
-switch (product[0])
+for (int i = 1; i <= 100; i++)
 {
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
+    if (i % 15 == 0)
+        suffix = " - FizzBuzz";
+    else if (i % 3 == 0)
+        suffix = " - Fizz";
+    else if (i % 5 == 0)
+        suffix = " - Buzz";
+    else
+        suffix = "";
 
-    default:
-        type = "Other";
-        break;   
+    Console.WriteLine($"{i}{suffix}");
 }
 
-switch (product[1])
-{
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;   
-}
-
-
-switch (product[2])
-{
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-
-    default:
-        size = "One Size Fits All";
-        break;   
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
-
-// ORIGINAL
-
-// SKU = Stock Keeping Unit
-// string sku = "01-MN-L";
-
-// string[] product = sku.Split('-');
-
-// string type = "";
-// string color = "";
-// string size = "";
-
-// switch (product[0])
-// {
-//     case "01":
-//         type = "Sweat shirt";
-//         break;
-//     case "02":
-//         type = "T-Shirt";
-//         break;
-//     case "03":
-//         type = "Sweat pants";
-//         break;
-//     default:
-//         type = "Other";
-//         break;
-// }
-
-// switch (product[1])
-// {
-//     case "BL":
-//         color = "Black";
-//         break;
-//     case "MN":
-//         color = "Maroon";
-//         break;
-//     default:
-//         color = "White";
-//         break;
-// }
-
-// switch (product[2])
-// {
-//     case "S":
-//         size = "Small";
-//         break;
-//     case "M":
-//         size = "Medium";
-//         break;
-//     case "L":
-//         size = "Large";
-//         break;
-//     default:
-//         size = "One Size Fits All";
-//         break;
-// }
-
-// Console.WriteLine($"Product: {size} {color} {type}");
