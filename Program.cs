5eefe983533c4d324bf12e2e4edb657e0bf438e5
Dayvid Ramos <d.ramosm97@gmail.com>
2025-07-25 10:34:07 -0500

    25-07-25: Challenge Hero vs Monster game

--- Program.cs
+++ Program.cs
@@ -1,10 +1,49 @@
-﻿// Exercise: Learn about Do-While and While loop
+﻿// Exercise: Complete a challenge activity using do and while iteration statements
 
-Random random = new Random();
-int current = 0;
+Random damage = new Random();
 
-do
+string monsterCharacter = "Monster";
+int monsterHealth = 10;
+
+string heroCharacter = "Hero";
+int heroHealth = 10;
+
+string currentTurn = heroCharacter;
+int nextCharacterHealth = 10;
+int healthLost = 0;
+
+
+string winner = "";
+
+//while (0 < monsterHealth && 0 < heroHealth)
+while (0 < nextCharacterHealth)
 {
-    current = random.Next(1, 11);
-    Console.WriteLine(current);
-} while (current != 7);
+    healthLost = damage.Next(1, 11);
+
+    if (currentTurn == heroCharacter)
+    {
+        monsterHealth -= healthLost;
+        nextCharacterHealth = monsterHealth;
+        if (nextCharacterHealth <= 0)
+            winner = heroCharacter;
+        
+        currentTurn = monsterCharacter;
+ 
+    }
+
+    else if (currentTurn == monsterCharacter)
+    {
+        heroHealth -= healthLost;
+        nextCharacterHealth = heroHealth;
+        if (nextCharacterHealth <= 0)
+            winner = monsterCharacter;
+        
+        currentTurn = heroCharacter;
+        
+    }
+
+    Console.WriteLine($"{currentTurn} was damaged and lost {healthLost} and now has {nextCharacterHealth} health.");
+}
+
+
+Console.WriteLine($"{winner} wins!");
