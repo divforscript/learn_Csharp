4497a38370e92bf40347d7e13a39a270ce7f990c
Dayvid Ramos <d.ramosm97@gmail.com>
2025-08-01 11:40:24 -0500

    01-08-25: Cast variable types exercise2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
 ﻿// Data types in C#
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-string message = "";
-float total = 0.0f;
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-foreach (string value in values)
-{
-    if (float.TryParse(value, out float validParse))
-    {
-        total += validParse;
-    }
-    else
-    {
-        message += value;
-    }
-}
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
