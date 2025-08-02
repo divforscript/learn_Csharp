fe04e658b92e592b769274d5e32f9112cf5a502c
Dayvid Ramos <d.ramosm97@gmail.com>
2025-08-02 17:25:12 -0500

    02-08-25: Challenge - Use string helper methods to format html code

--- Program.cs
+++ Program.cs
@@ -1,33 +1,21 @@
-﻿// Exercise: Complete a challenge to apply string interpolation to a form letter
+﻿// Exercise: Complete a challenge to extract, replace, and remove data from an input string
 
-string customerName = "Ms. Barros";
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+string quantity = "";
+string output = "";
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
+// Your work here
 
-// Your logic here
+int openSpanIndex = input.IndexOf("<s");
+int closeSpanIndex = input.IndexOf("</s");
+quantity = "Quantity: " + input.Substring(openSpanIndex + 6, 4);
 
-Console.WriteLine(
-    $"Dear {customerName},\n\n" +
-    $"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n\n" +
-    
-    $"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n\n" + 
-    
-    $"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n"
-);
+// Set the output
+output = input.Replace("<div>", "");
+output = output.Replace("</div>", "");
+output = "Output: " + output.Replace("&trade", "&reg");
 
-Console.WriteLine("Here's a quick comparison:\n");
+Console.WriteLine(quantity);
+Console.WriteLine(output);
 
-string comparisonMessage = "";
-
-// Your logic here
-comparisonMessage =
-currentProduct.PadRight(20) + $"{currentReturn:P2}".PadRight(9) + $"{currentProfit:C2}\n" + newProduct.PadRight(20) + $"{newReturn:P2}".PadRight(9) + $"{newProfit:C2}";
-
-Console.WriteLine(comparisonMessage);
