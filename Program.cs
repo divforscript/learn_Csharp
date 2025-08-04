5f6b45ab4e8dcaf85dd361056ca51f09f176f653
Dayvid Ramos <d.ramosm97@gmail.com>
2025-08-03 22:50:17 -0500

    03-08-25: Exercise - Named and optional parameters

--- Program.cs
+++ Program.cs
@@ -1,54 +1,46 @@
-﻿// Exercise - Use value and reference type parameters
+﻿// Exercise - Methods with optional parameters
 
-// By value
-int a = 3;
-int b = 4;
-int c = 0;
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
 
-void Multiply(int a, int b, int c)
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
 {
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
-}
-
-
-// By reference - Arrays
-int[] array = {1, 2, 3, 4, 5};
-
-PrintArray(array);
-Clear(array);
-PrintArray(array);
-
-void PrintArray(int[] array)
-{
-    foreach (int a in array)
+    if (inviteOnly)
     {
-        Console.Write($"{a} ");
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
     }
-    Console.WriteLine();
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
 }
 
-
-void Clear(int[] array)
+void ShowRSVPs()
 {
-    for (int i = 0; i < array.Length; i++)
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
     {
-        array[i] = 0;
+        Console.WriteLine(rsvps[i]);
     }
-}
-
-// By reference - Strings
-string status = "Healthy";
-
-Console.WriteLine($"Start: {status}");
-SetHealth(false);
-Console.WriteLine($"End: {status}");
-
-void SetHealth(bool isHealthy) 
-{
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
 }
