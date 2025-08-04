f74265c6fd6a65e0a4beda8127e99dc7d0925fc8
Dayvid Ramos <d.ramosm97@gmail.com>
2025-08-03 23:29:44 -0500

    03-08-25: Exercise - Complete the challenge to display email addresses

--- Program.cs
+++ Program.cs
@@ -1,46 +1,36 @@
-﻿// Exercise - Methods with optional parameters
+﻿// Exercise - Complete the challenge to display email addresses
 
-string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
-string[] rsvps = new string[10];
-int count = 0;
-
-RSVP("Rebecca");
-RSVP("Nadia", 2, "Nuts");
-RSVP(name: "Linh", partySize: 2, inviteOnly: false);
-RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
-RSVP("Noor", 4, inviteOnly: false);
-RSVP("Jonte", 2, "Stone fruit", false);
-ShowRSVPs();
-
-void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+string[,] corporate = 
 {
-    if (inviteOnly)
-    {
-        bool found = false;
-        foreach (string guest in guestList)
-        {
-            if (guest.Equals(name))
-            {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-        {
-            Console.WriteLine($"Sorry, {name} is not on the guest list");
-            return;
-        }
-    }
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
 
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    // display internal email addresses
+    DisplayEmail(corporate[i,0],corporate[i,1]);
 }
 
-void ShowRSVPs()
+for (int i = 0; i < external.GetLength(0); i++)
 {
-    Console.WriteLine("\nTotal RSVPs:");
-    for (int i = 0; i < count; i++)
-    {
-        Console.WriteLine(rsvps[i]);
-    }
+    // display external email addresses
+    DisplayEmail(external[i,0],external[i,1],externalDomain);
+}
+
+
+void DisplayEmail(string userName, string userLastName, string domain = "contoso.com")
+{
+    string emailAddress = $"{userName.Substring(0, 2).ToLower()}{userLastName.ToLower()}@{domain}";
+
+    Console.WriteLine(emailAddress);
 }
