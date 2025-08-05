5d54dbf3e9eb88aa535a76e548bf5a9d4752cc07
Dayvid Ramos <d.ramosm97@gmail.com>
2025-08-05 16:46:23 -0500

    05-08-25: Challenge Project - Create a Mini Game, Part 1

--- Program.cs
+++ Program.cs
@@ -1,81 +1,208 @@
-﻿// Guided Project - Contoso Petting Zoo
+﻿// Challenge Project - Create a Mini Game
 
 /*
-- There will be three visiting schools
-    - School A has six visiting groups (the default number)
-    - School B has three visiting groups
-    - School C has two visiting groups
+- The code declares the following variables:
+    - Variables to determine the size of the Terminal window.
+    - Variables to track the locations of the player and food.
+    - Arrays `states` and `foods` to provide available player and food appearances
+    - Variables to track the current player and food appearance
 
-- For each visiting school, perform the following tasks
-    - Randomize the animals
-    - Assign the animals to the correct number of groups
-    - Print the school name
-    - Print the animal groups
+- The code provides the following methods:
+    - A method to determine if the Terminal window was resized.
+    - A method to display a random food appearance at a random location.
+    - A method that changes the player appearance to match the food consumed.
+    - A method that temporarily freezes the player movement.
+    - A method that moves the player according to directional input.
+    - A method that sets up the initial game state.
+
+- The code doesn't call the methods correctly to make the game playable. The following features are missing:
+    - Code to determine if the player has consumed the food displayed.
+    - Code to determine if the food consumed should freeze player movement.
+    - Code to determine if the food consumed should increase player movement.
+    - Code to increase movement speed.
+    - Code to redisplay the food after it's consumed by the player.
+    - Code to terminate execution if an unsupported key is entered.
+    - Code to terminate execution if the terminal was resized.
 */
+
+// Specifications
+
+/*
+Terminate on resize
+This feature must:
+
+- Determine if the terminal was resized before allowing the game to continue
+- Clear the Console and end the game if the terminal was resized
+- Display the following message before ending the program: Console was resize. Program exiting.
+*/
+
+
+/*
+Add optional termination
+- Modify the existing Move method to support an optional parameter
+- If enabled, the optional parameter should detect nondirectional key input
+- If nondirectional input is detected, allow the game to terminate
+*/
+
 
 using System;
 
-string[] pettingZoo =
+Random random = new Random();
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
+
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
+
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
+
+// Available player and food strings
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
+
+// Current player string displayed in the Console
+string player = states[0];
+
+// Index of the current food
+int food = 0;
+
+
+////////////////////////
+// Game Logic
+///////////////////////
+
+InitializeGame();
+while (!shouldExit)
 {
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
-    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
-};
+    // Without parameter: Accept any key
+    Move();
 
-PlanSchoolVisit("School A");
-PlanSchoolVisit("School B", 3);
-PlanSchoolVisit("School C", 2);
+    // With parameter: End the game if no-directional key was pressed
+    // Move("restrict");
+}
+Console.CursorVisible = true;
+/////////////////////////
 
 
-void PlanSchoolVisit(string schoolName, int groups = 6)
+
+
+////////////////////////
+/// Functions
+///////////////////////
+
+// Returns true if the Terminal was resized 
+bool TerminalResized()
 {
-    RandomizeAnimals();
-    string[,] group = AssignGroup(groups);
-    Console.WriteLine(schoolName);
-    PrintGroup(group);
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
-
-void RandomizeAnimals()
+// Displays random food at a random location
+void ShowFood()
 {
-    Random random = new Random();
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
 
-    for (int i = 0; i < pettingZoo.Length; i++)
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
+}
+
+// Changes the player to match the food consumed
+void ChangePlayer()
+{
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer()
+{
+    System.Threading.Thread.Sleep(1000);
+    player = states[0];
+}
+
+// Reads directional input from the Console and moves the player
+void Move(string mode = "f")
+{
+    bool endGame = false;
+    int lastX = playerX;
+    int lastY = playerY;
+
+    switch (Console.ReadKey(true).Key)
     {
-        int r = random.Next(i, pettingZoo.Length);
-
-        string temp = pettingZoo[i];
-        pettingZoo[i] = pettingZoo[r];
-        pettingZoo[r] = temp;
+        case ConsoleKey.UpArrow:
+            playerY--;
+            break;
+        case ConsoleKey.DownArrow:
+            playerY++;
+            break;
+        case ConsoleKey.LeftArrow:
+            playerX--;
+            break;
+        case ConsoleKey.RightArrow:
+            playerX++;
+            break;
+        case ConsoleKey.Escape:
+            shouldExit = true;
+            break;
+        default:
+            endGame = mode.Equals("restrict");
+            break;
     }
-}
 
-string[,] AssignGroup(int groups = 6)
-{
-    string[,] result = new string[groups, pettingZoo.Length / groups];
-
-    int start = 0;
-
-    for (int i = 0; i < groups; i++)
+    if (endGame)
     {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            result[i, j] = pettingZoo[start++];
-        }
+        shouldExit = true;
+        Console.Clear();
+        return;
     }
 
-    return result;
-}
-
-void PrintGroup(string[,] group) 
-{
-    for (int i = 0; i < group.GetLength(0); i++) 
+    if (TerminalResized())
     {
-        Console.Write($"Group {i + 1}: ");
-        for (int j = 0; j < group.GetLength(1); j++) 
-        {
-            Console.Write($"{group[i,j]}  ");
-        }
-        Console.WriteLine();
+
+        Console.Clear();
+
+        // Write message before exiting game
+        Console.SetCursorPosition(0, 0);
+        Console.WriteLine("Console was resized. Program exiting.");
+        shouldExit = true;
+        Thread.Sleep(1500);
+        return;
     }
+
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++)
+    {
+        Console.Write(" ");
+    }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
+    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
 }
+
+// Clears the console, displays the food and player
+void InitializeGame()
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
+}
+
+
